bbf163a80d023d4c696b9c37c52238299d2bcf46
hrmeheraj2021 <hrmeheraj2021@gmail.com>
2022-05-25 09:05:38 -0400

    Dashboard Index page Set up

--- src/components/pages/Dashboard/IndexDashboard.jsx
+++ src/components/pages/Dashboard/IndexDashboard.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase/firebase.config'
 import Loading from '../../Shared/Loading/Loading';
-
+import { MdSpaceDashboard } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 function IndexDashboard() {
     const [user, loading] = useAuthState(auth);
-    if(user){
-        console.log(user);
-    }
     return (
-        <div>
-            {
-                loading && <Loading/>
-            }
-            <h2 className='text-5xl mb-5 text-primary text-center mt-[20px]'> WELCOME TO DASHBOARD!</h2>
-            {
-                user && <h3> Hi, {user?.displayName}</h3>
-            }
+        <div className='max-w-[720px] w-[95%] mx-auto mt-[20px] shadow-sm rounded-md'>
+         <div className='flex justify-around items-center py-2 shadow-xl rounded-full'>
+                <h2 className='text-xl font-semibold text-primary'> Hi , { user?.displayName} </h2>
+                <label for="" className='text-2xl'> <MdSpaceDashboard/> </label>
+            </div>
+            <div className='h-[300px] shadow-md w-[95%] mx-auto w-full flex justify-center items-center mt-[40px] mb-[40px]'> 
+                <div className='text-center'>
+                    <h2 className='py-2 text-2xl font-bold text-primary'> Welcome to Dashboard! </h2> 
+                    <p className='font-semibold text-gray-600'>Explore All of the Features and Give a Review. </p>
+                    <Link to='orders' className='mt-[15px] btn '>My Orders  </Link>
+                </div>
+            </div>
         </div>
     )
 }
